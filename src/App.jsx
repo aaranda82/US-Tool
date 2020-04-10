824caf9dfe7d75c6d2be3f09c6d4c8a996123c65
Alex <alexraranda@gmail.com>
2020-04-10 16:52:46 -0700

    restarted page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,48 @@
 import React from 'react';
-import './App.css';
-import Header from './Header';
-import Nav from './Nav';
-import About from './About';
-import Services from './Services';
 
 function App() {
   return (
     <>
-      <div className="top">
-        <Header />
-        {/* <Nav /> */}
-      </div>
-      <div className="body">
-        <About />
-        <Services />
-      </div>
+      <header>
+        <div>
+          <a href="mailto:info@ustooldie.com">info@ustooldie.com</a>
+        </div>
+        <div>
+          <img src="logo_transparent.png" alt="" /> US Tool & Die
+        </div>
+        <div>
+          <a href="tel:19494549858">(949) 454 - 9858</a>
+        </div>
+      </header>
+      <main>
+        <div className="about">
+          We are a family owned and operated business in Laguna Hills with over
+          20 years of machining experience. Our focus is on excellent customer
+          service and quality production.
+        </div>
+        <div className="services">Services</div>
+        <div className="servicesList">
+          <div>CNC Mills</div>
+          <div>CNC Lathes</div>
+          <div>Wire EDMs</div>
+          <div>Presses</div>
+        </div>
+        <div className="contact">Contact Us</div>
+        <div className="contactInfo">
+          <div className="address">
+            23274 Del Lago Dr #1308,
+            <br />
+            Laguna Hills,
+            <br />
+            CA 92653
+          </div>
+          <div className="emailPhone">
+            <a href="mailto:info@ustooldie.com">info@ustooldie.com</a>
+            <br />
+            <a href="tel:19494549858">(949) 454 - 9858</a>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
