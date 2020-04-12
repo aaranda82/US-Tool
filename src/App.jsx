5eb674f28a9b83a352ed802de3a5081f8ea217ce
Alex <alexraranda@gmail.com>
2020-04-12 13:31:51 -0700

    cleaned up App page

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
 import Background from './components/Background';
 
 function App() {
-  const Header = styled.div`
-    color: #dfe6e9;
-    border: #dfe6e9 3px solid;
-    height: 10vh;
-  `;
-
-  const Quote = styled.div`
-    width: 40%;
-    height: 30vh;
-    color: #dfe6e9;
-    border: #dfe6e9 3px solid;
-    display: inline-block;
-  `;
-  const Services = styled.div`
-    height: 30vh;
-    color: #dfe6e9;
-    border: #dfe6e9 3px solid;
-  `;
-
   return <Background />;
 }
 
