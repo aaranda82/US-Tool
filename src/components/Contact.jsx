ad95d445e080ac72fd7fe91fa5fcab3ce3a6324d
Alex <alexraranda@gmail.com>
2020-04-24 17:21:19 -0700

    updated form validation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,7 @@ const ContactForm = styled.div`
   color: ${lightGrey};
   border: ${lightGrey} 3px solid;
   width: 25%;
+  min-height: 660px;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
     Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   @media (max-width: 400px) {
@@ -33,12 +34,17 @@ const Button = styled.button`
   font-size: 20px;
   border: none;
   background-color: ${salmon};
+  font-weight: bold;
+  padding: 10px 20px 10px 20px;
   &:active {
     transform: scale(1.2, 1.2);
   }
   &:hover {
     background-color: ${red};
   }
+`;
+const Legend = styled.legend`
+  text-align: center;
 `;
 
 class Contact extends React.Component {
@@ -61,6 +67,10 @@ class Contact extends React.Component {
     this.handleOnChangeEmail = this.handleOnChangeEmail.bind(this);
     this.handleOnChangeText = this.handleOnChangeText.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.validateFirstName = this.validateFirstName.bind(this);
+    this.validateLastName = this.validateLastName.bind(this);
+    this.validateEmail = this.validateEmail.bind(this);
+    this.validateText = this.validateText.bind(this);
   }
 
   handleRender() {
@@ -68,12 +78,14 @@ class Contact extends React.Component {
       <ContactForm>
         <Form onSubmit={this.handleSubmit}>
           <fieldset>
+            <Legend>Contact Us</Legend>
             <label htmlFor="firstName">First Name:</label>
             <Input
               id="firstName"
               type="text"
               onChange={this.handleOnChangeFirstName}
               value={this.state.firstName}
+              onBlur={this.validateFirstName}
             />
             <Error>{this.state.errors.firstNameError}</Error>
             <label htmlFor="lastName">Last Name:</label>
@@ -82,6 +94,7 @@ class Contact extends React.Component {
               type="text"
               onChange={this.handleOnChangeLastName}
               value={this.state.lastName}
+              onBlur={this.validateLastName}
             />
             <Error>{this.state.errors.lastNameError}</Error>
             <label htmlFor="email">Email:</label>
@@ -90,6 +103,7 @@ class Contact extends React.Component {
               type="text"
               onChange={this.handleOnChangeEmail}
               value={this.state.email}
+              onBlur={this.validateEmail}
             />
             <Error>{this.state.errors.emailError}</Error>
             <label htmlFor="message">Message:</label>
@@ -99,6 +113,7 @@ class Contact extends React.Component {
               value={this.state.text}
               cols="30"
               rows="10"
+              onBlur={this.validateText}
             ></Text>
             <Error>{this.state.errors.textError}</Error>
             <Button type="submit">Submit</Button>
@@ -124,37 +139,58 @@ class Contact extends React.Component {
     this.setState({ text: e.target.value });
   }
 
-  handleSubmit(e) {
-    e.preventDefault();
+  validateFirstName() {
     let errors = { ...this.state.errors };
-    const { firstName, lastName, email, text } = this.state;
-    // eslint-disable-next-line
-    const reg = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    const { firstName } = this.state;
     if (!firstName) {
       errors.firstNameError = 'Please enter a first name';
     } else {
       errors.firstNameError = '';
     }
+    this.setState({ errors });
+  }
 
+  validateLastName() {
+    let errors = { ...this.state.errors };
+    const { lastName } = this.state;
     if (!lastName) {
       errors.lastNameError = 'Please enter a last name';
     } else {
       errors.lastNameError = '';
     }
+    this.setState({ errors });
+  }
 
+  validateEmail() {
+    let errors = { ...this.state.errors };
+    const { email } = this.state;
+    // eslint-disable-next-line
+    const reg = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
     if (!email) {
-      errors.emailError = 'Please enter an email';
+      errors.emailError = 'Please enter an email address';
     } else if (!reg.test(email)) {
-      errors.emailError = 'Please enter a valid email address';
+      errors.emailError = 'Email address not valid';
     } else {
       errors.emailError = '';
     }
+    this.setState({ errors });
+  }
+
+  validateText() {
+    let errors = { ...this.state.errors };
+    const { text } = this.state;
     if (!text) {
       errors.textError = 'Please enter a message';
     } else {
       errors.textError = '';
     }
+    this.setState({ errors });
+  }
 
+  handleSubmit(e) {
+    e.preventDefault();
+    let errors = { ...this.state.errors };
+    const { firstName, lastName, email, text } = this.state;
     if (firstName && lastName && email && text) {
       console.log('I submit!');
       errors = {
